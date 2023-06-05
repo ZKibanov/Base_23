9bdd948825c078554befe9dbe6c79320f0254906
ZKibanov <68987481+ZKibanov@users.noreply.github.com>
2023-06-05 16:51:44 +0300

    Add files via upload

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import './index.scss'
+import { Counter } from "./Counter"
+const App = () => {
+    return (
+        <div className='app'>
+            Hello Ulbi
+            <Counter />
+        </div>
+    )
+}
+
+export default App;
